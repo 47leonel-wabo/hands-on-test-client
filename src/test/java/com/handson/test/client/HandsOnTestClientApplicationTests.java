6778f1c709b1d8ad8a51cf7d2d87ca84e7248dab
leonel47 <waboleonel@gmail.com>
2021-08-27 19:37:34 -0400

    Initial commit, project sets

---
+++ src/test/java/com/handson/test/client/HandsOnTestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handson.test.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnTestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
